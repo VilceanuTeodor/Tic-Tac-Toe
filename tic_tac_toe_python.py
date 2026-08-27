7bd4e3e37876124f84a2628574436776da5c112e
DrPyroBlazer <teodor.vilceanu11@gmail.com>
2026-08-27 17:09:34 +0300

    Commit 4; Processed Player's moves on the game's logic

--- tic_tac_toe_python.py
+++ tic_tac_toe_python.py
@@ -38,6 +38,57 @@ class TicTacToeGame:
         ]
         self._winning_combos = self._get_winning_combos()
 
+    def _get_winning_combos(self):
+        rows = [
+        [(move.row, move.col) for move in row]
+        for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+    def is_valid_move(self, move):
+        #Return True if the move is valid and False if it isn't
+
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label = ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+
+    def process_move(self, move):
+        # Process the current move to check if it is a win.
+
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_combos:
+            results = set(
+                self._current_moves[n][m].label
+                for n,m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+
+    def has_winner(self):
+        # Return True if the game has a winner, False if it doesn't
+        return self._has_winner
+
+    def is_tied(self):
+        # Return True if there is a tie, False if there isn't
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all(played_moves)
+
+    def toggle_player(self):
+        # Return a toggled player
+        self.current_player = next(self._players)
+    
+
 class TicTacToeBoard(tk.Tk):
     def __init__(self):
         super().__init__()
